dbae4e07e6e4c4be58491e099ba58780b8deab72
Nicolas-Guilbaud <Nicolas.Guilbaud@ulb.be>
2026-05-25 16:25:19 +0200

    fast cost with cpu cast

---
+++ kernels/optimized/fast_convol_cpu_cast.cu
@@ -0,0 +1,224 @@
+#include "fast_convol_cpu_cast.cuh"
+
+#define divup(x,y) (((x)+(y)-1)/(y))
+
+inline __global__ void convolution_2D_naive_kernel(
+    uint8_t *input,
+    uint8_t *output,
+    int width,
+    int height,
+    int window
+){
+    int x = threadIdx.x + blockDim.x * blockIdx.x,
+        y = threadIdx.y + blockDim.y * blockIdx.y;
+    
+    if(x >= width || y >= height)
+        return;
+    
+    int sum = 0;
+    
+    for(int k = -window/2; k < window/2; k++){
+        for(int l = -window/2; l < window/2; l++){
+
+            if(x+l < 0 || x+l >= width)
+                continue;
+            if(y+k < 0 || y+k >= height)
+                continue;
+            sum += input[(x+l) + width*(y+k)];
+        }
+    }
+
+    output[x + width*y] = sum/window;
+}
+
+
+__global__ void single_cam_fast_cost_fp32_kernel(
+
+    const float* A,
+    const float* B,
+
+    const uint8_t* ref_Y_img, //ref
+    const uint8_t* current_Y_img,
+
+    const int width,
+    const int height,
+
+	const int window,
+	float *cost_mat
+){
+    int x = threadIdx.x + blockIdx.x * blockDim.x,
+		y = threadIdx.y + blockIdx.y * blockDim.y,
+        zIdx = threadIdx.z + blockIdx.z * blockDim.z;
+    
+    if(x >= width || y >= height || zIdx >= ZPlanes){
+        return;
+    }
+
+    // Calculate z from ZNear, ZFar and ZPlanes (projective transformation) (zi = 0, z = ZFar)
+    float z = ZNear * ZFar / (ZNear + (((float)zIdx / (float)ZPlanes) * (ZFar - ZNear)));
+
+    float X_proj = ( A[0] * x +  A[1] * y +  A[2]) * z -  B[0],
+        Y_proj = ( A[3] * x +  A[4] * y +  A[5]) * z -  B[1],
+        Z_proj = ( A[6] * x +  A[7] * y +  A[8]) * z -  B[2];
+
+    float x_proj = X_proj / Z_proj,
+        y_proj = Y_proj / Z_proj;
+    
+    x_proj = x_proj < 0 || x_proj >= width ? 0 : roundf(x_proj);
+    y_proj = y_proj < 0 || y_proj >= height ? 0 : roundf(y_proj);
+    // (ii) calculate cost against reference
+    // Calculating cost in a window
+    float cost = fabsf(ref_Y_img[x + y*width] - current_Y_img[(int) (x_proj + y_proj*width)]);
+    float min = cost_mat[(x + width*(y + height*(zIdx)))];
+
+    cost_mat[(x + width*(y + height*(zIdx)))] = fminf(cost,min);
+}
+
+std::vector<cv::Mat> single_cam_fast_convol_cpu_cast(
+    int ref_idx,
+    std::vector<cam> const cam_vector,
+    int window
+){
+    std::vector<cv::Mat> result(ZPlanes);
+
+    cam ref = cam_vector.at(ref_idx);
+    int width = ref.width,
+        height = ref.height;
+
+    int cam_vec_size = cam_vector.size();
+
+    //CPUrows
+    std::vector<float> host_cost_mat(width*height*ZPlanes,255.f);
+    //GPU
+    float *dev_A,*dev_B;
+    
+    float *dev_cost_mat;
+    uint8_t *ref_Y_img, *current_Y_img, *convol_buffer;
+
+    //threads & blocks:
+
+
+    int block_x = divup(width,max_threads_512.x),
+        block_y = divup(height,max_threads_512.y),
+        block_z = divup(ZPlanes,max_threads_512.z);
+    
+    dim3 convol_blocks(block_x,block_y);
+    
+    dim3 N_blocks(block_x,block_y,block_z);
+
+    CHK(cudaSetDevice(0));
+
+    //init pointers
+    CHK(cudaMalloc(&dev_cost_mat,width*height*ZPlanes*sizeof(float)));
+    CHK(cudaMalloc(&dev_A,9*sizeof(float)));
+    CHK(cudaMalloc(&dev_B,3*sizeof(float)));
+
+
+    CHK(cudaMalloc(&ref_Y_img,width*height*sizeof(uint8_t)));
+    CHK(cudaMalloc(&current_Y_img,width*height*sizeof(uint8_t)));
+    CHK(cudaMalloc(&convol_buffer,width*height*sizeof(uint8_t)));
+
+    //cpy ref cam to convolution buffer
+    CHK(cudaMemcpy(convol_buffer,ref.YUV[0].data,width*height*sizeof(uint8_t),cudaMemcpyHostToDevice));
+    //run 2D cnvolution & store as ref img
+    convolution_2D_naive_kernel<<<convol_blocks,max_threads_512>>>(
+        convol_buffer,
+        ref_Y_img,
+        width,
+        height,
+        window
+    );
+
+    //wait for convolution to finish
+    CHK(cudaDeviceSynchronize());
+
+    //init cost mat:
+    cudaMemcpy(dev_cost_mat,&host_cost_mat[0],width*height*ZPlanes*sizeof(float),cudaMemcpyHostToDevice);
+
+    for(int cam_idx = 0; cam_idx < cam_vec_size; cam_idx++){
+
+        //skip ref cam
+        if(ref_idx == cam_idx){
+            continue;
+        }
+
+        cam curr = cam_vector.at(cam_idx);
+
+        /* matmult on CPU: */
+        cv::Mat K = cv::Mat(3,3,CV_64F,&curr.p.K[0]), 
+            K_inv = cv::Mat(3,3,CV_64F,&ref.p.K_inv[0]), 
+            R = cv::Mat(3,3,CV_64F,&curr.p.R[0]), 
+            R_inv = cv::Mat(3,3,CV_64F,&ref.p.R_inv[0]), 
+            t = cv::Mat(3,1,CV_64F,&curr.p.t[0]), 
+            t_inv = cv::Mat(3,1,CV_64F,&ref.p.t_inv[0]);
+
+        cv::Mat host_A = K*R*R_inv*K_inv,
+            host_B = K*((R*t_inv) + t);
+        
+        //cast into 32-bit float
+        host_A.convertTo(host_A,CV_32F);
+        host_B.convertTo(host_B,CV_32F);
+
+        float *a = host_A.ptr<float>(),
+            *b = host_B.ptr<float>();
+
+        //copy homogeneous matrices
+        cudaMemcpy(dev_A,a,9*sizeof(float),cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_B,b,3*sizeof(float),cudaMemcpyHostToDevice);
+        
+        //copy Y img to convol buffer
+        CHK(cudaMemcpy(convol_buffer,curr.YUV[0].data,width*height*sizeof(uint8_t),cudaMemcpyHostToDevice));
+        
+        //run 2D convolution & store as current img
+        convolution_2D_naive_kernel<<<convol_blocks,max_threads_512>>>(
+            convol_buffer,
+            current_Y_img,
+            width,
+            height,
+            window
+        );
+
+        //wait for convolution to finish
+        CHK(cudaDeviceSynchronize());
+
+        //run kernel
+        single_cam_fast_cost_fp32_kernel<<<N_blocks,max_threads_512>>>(
+            dev_A,dev_B,
+            ref_Y_img,
+            current_Y_img,
+            width,
+            height,
+            window,
+            dev_cost_mat
+        );
+
+        CHK(cudaDeviceSynchronize());
+    }
+
+    CHK(cudaMemcpy(&host_cost_mat[0],dev_cost_mat,width*height*ZPlanes*sizeof(float),cudaMemcpyDeviceToHost));
+
+    //copy back in cv mat format
+    for(int zi = 0; zi < ZPlanes; zi++){
+
+        float* host_cost_zi = &host_cost_mat[zi*width*height];
+
+        result.at(zi) = cv::Mat(
+            height, 
+            width, 
+            CV_32FC1,
+            host_cost_zi
+        ).clone();
+    }
+
+Error:
+
+    //free cuda pointers
+    cudaFree(dev_A);
+    cudaFree(dev_B);
+    cudaFree(ref_Y_img);
+    cudaFree(current_Y_img);
+
+    cudaFree(dev_cost_mat);
+
+    return result;
+}
